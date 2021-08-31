cd942ffc582c6922f1717b2ee5107e73628d0c13
Chirag <Chiragsimkhada@gmail.com>
2021-08-31 23:35:26 +0545

    Added Validators, messages to registration page

--- LaCinephile/accounts/models.py
+++ LaCinephile/accounts/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core import validators
+from django.core.validators import *
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20, validators=[validators.MinLengthValidator(3)])
+    last_name = models.CharField(max_length=20,validators=[validators.MinLengthValidator(3)])
     user_name = models.CharField(max_length=20, unique=True)
-    email = models.EmailField()
-    password = models.CharField(max_length=30)
+    email = models.EmailField(validators=[validate_email])
+    password = models.CharField(max_length=30,validators=[validators.MinLengthValidator(8)])
     date_created = models.DateField(auto_created=True, auto_now=True, null=True)
 
     def __str__(self):
@@ -20,8 +21,8 @@ class User(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     dob = models.DateField(auto_now=False, auto_now_add=False, null=True)
-    middle_name = models.CharField(max_length=20, null=True, blank=True)
-    pic = models.ImageField(upload_to ='uploads/profile', null=True, default="static/images/sample_user.png")
-    phone = models.CharField(max_length=10, null=True)
+    middle_name = models.CharField(max_length=20, null=True, blank=True, validators=[validators.MinLengthValidator(3)])
+    pic = models.ImageField(upload_to ='uploads/profile', null=True, default="static/images/sample_user.png", validators=[validate_image_file_extension])
+    phone = models.CharField(max_length=10, null=True,validators=[validators.MinLengthValidator(7)])
     
 
--- LaCinephile/accounts/views.py
+++ LaCinephile/accounts/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+import re
+from django.contrib.auth import forms
+from django.shortcuts import redirect, render
 from .forms import UserForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-    
+    form = UserForm()
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
+            print("Valid")
             form.save()
-    else:
-        form = UserForm()
+            return redirect('/')
+        else:
+            messages.add_message(request, messages.ERROR,"User Registration Failed!")
+            return render(request, 'accounts/register.html', {'form':form})
+        
     context={
         'form':form
     }
