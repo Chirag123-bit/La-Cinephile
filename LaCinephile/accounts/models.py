f0ac651349bdd888eaf03ea61c8421e93f5b0c3f
Chirag <Chiragsimkhada@gmail.com>
2021-09-22 22:17:54 +0545

    About to change user profile

--- LaCinephile/accounts/models.py
+++ LaCinephile/accounts/models.py
@@ -1,13 +1,15 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import *
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, default=None)
+    # username = models.CharField(max_length=50, default=None)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
     district = models.CharField(max_length=30, default="Kathmandu")
@@ -18,5 +20,10 @@ class Profile(models.Model):
     def __str__(self):
         return self.firstname + " "+ self.lastname
 
-    
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile  = Profile.objects.create(user=kwargs['instance'])
+post_save.connect(create_profile, sender=User)
+
 
